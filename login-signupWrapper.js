c9faed905caddde82078dd23f3ea3d6a461f3474
Juanesor11 <juanesor07@gmail.com>
2024-08-25 17:14:38 -0300

    added post review

--- login-signupWrapper.js
+++ login-signupWrapper.js
@@ -3,9 +3,8 @@ let logInBox = document.querySelector(".logInBox");
 let signUpBox = document.querySelector(".signUpBox");
 let loginLink = document.querySelector(".loginLink");
 let signUpLink = document.querySelector(".signUpLink");
-let registerBtn = document.querySelector("registerBtn");
 
-signUpLink.addEventListener("click", () =>{
+signUpLink.addEventListener("click", () => {
     signUpBox.style.display = "block"
     logInBox.style.display = "none"
     wrapper.style.height = "440px"
---
+++ postReview.js
@@ -0,0 +1,23 @@
+let postReviewBTN = document.querySelector('#postReviewBTN')
+let wrapper = document.querySelector('.reviewPost')
+
+let x = document.querySelector('.close')
+
+let form = document.querySelector('.postReviewForm')
+let btn = document.querySelector('.postButton')
+let post = document.querySelector('.post')
+let contenedor = document.querySelector('.contenedorEstrellas')
+
+postReviewBTN.addEventListener('click', () => {
+    wrapper.style.display = 'block'
+})
+
+x.addEventListener('click', () => {
+    wrapper.style.display = 'none'
+})
+
+btn.addEventListener('click', () => {
+    contenedor.style.display = 'none'
+    form.style.display = 'none'
+    post.style.display = 'block'
+})
